3e105d0633e3ed83234c402fab8ce3c4306198ef
Tyler Carbone <tcarbone073@gmail.com>
2023-12-27 19:01:09 +0000

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  platforms: Platform[];
+}
+
+function PlatformIconList({ platforms }: Props) {
+  const iconsMap: { [key: string]: IconType } = {
+    android: FaAndroid,
+    mac: FaApple,
+    ios: MdPhoneIphone,
+    linux: FaLinux,
+    nintendo: SiNintendo,
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    web: BsGlobe,
+    xbox: FaXbox,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconsMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
