052f0d62f57578174ec95cdf86d2c8e8c006c6ab
Tyler Carbone <tcarbone073@gmail.com>
2023-12-27 10:57:55 +0000

    Add ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -7,6 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
