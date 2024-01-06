f1b90d86447718441c6efdb07b5f1c9071e37c96
Tyler Carbone <tcarbone073@gmail.com>
2024-01-06 14:45:21 +0000

    Fetch genres using react-query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 import apiClient from "../services/api-client";
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -7,7 +11,12 @@ export interface Genre {
 }
 
 function useGenres() {
-  return { data: genres, isLoading: null, error: null };
+  return useQuery({
+    queryKey: ["genres"],
+    queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: genres.length, results: genres },
+  });
 }
 
 export default useGenres;
