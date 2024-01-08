819f96a0f536b9a7419a0b3567eebaad75dbab44
Tyler Carbone <tcarbone073@gmail.com>
2024-01-08 23:26:40 +0000

    Ship platforms with app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
+import platforms from "../data/platforms";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
 
@@ -17,6 +18,7 @@ function usePlatforms() {
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
   });
 }
 
