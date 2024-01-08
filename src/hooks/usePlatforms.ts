a38e79f0d063884f7af29ce09ce40c89992e59ed
Tyler Carbone <tcarbone073@gmail.com>
2024-01-08 23:18:05 +0000

    Fetch platforms using react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Platform {
   id: number;
@@ -7,7 +10,14 @@ export interface Platform {
 }
 
 function usePlatforms() {
-  return useData<Platform>("/platforms/lists/parents");
+  return useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+  });
 }
 
 export default usePlatforms;
